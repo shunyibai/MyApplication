e7df438cc2897c52f9b816d4ac2d20b45054a55d
origin <https://github.com/shunyibai/face.git>
2018-03-10 21:27:01 +0800

    初次创建

--- app/src/main/java/com/example/a81940/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a81940/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.a81940.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv_hello_mainAty);
     }
 }
