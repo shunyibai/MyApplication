9ed222d6e2a28c563950f9e24aaebdda8b0445b4
origin <https://github.com/shunyibai/face.git>
2018-03-10 21:55:00 +0800

    创建相机界面布局

--- app/src/main/java/com/example/a81940/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a81940/myapplication/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv;
+    private TextView tv;///build
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv_hello_mainAty);
+
+        tv.setText("nihao");
     }
 }
